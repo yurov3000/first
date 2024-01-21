01844b21b665a3cbe14dd7fe3b7dda5b12c4ad9e
Danil <62295734+yurov3000@users.noreply.github.com>
2024-01-21 13:45:06 +0300

    Created first app on C#.
    This my first app on C#, i am joing learned C#.

---
+++ Project1/Project1/Program.cs
@@ -0,0 +1,28 @@
+﻿// Использование пространства имён
+using System;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+// класс с главным методом программы
+class InputConsoleDemo 
+{
+    // Главный метод программы
+    static void Main() 
+    {
+        string txt, txtMes, txtTitle = "Расчёт соседних чисел";
+        int res1, res2, txtInt;
+
+        txt = Interaction.InputBox("Введите любое число");
+
+        txtInt = Int32.Parse(txt);
+        res1 = txtInt - 1;
+        res2 = txtInt + 1;
+
+        txtMes = $"Тройка чисел: {res1} - {txtInt} - {res2}";
+
+        MessageBox.Show(txtMes, txtTitle, MessageBoxButtons.OK);
+    }
+}
+
+
+// Закончили на 60 странице. Васильев C#.
